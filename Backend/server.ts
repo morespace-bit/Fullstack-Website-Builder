6e04d28099ac3c255f95080db729b777a78a3198
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-05-30 03:11:16 +0545

    new project setup

--- Backend/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./src/app";
-import { dotEnv } from "./src/config/config";
-
-const serverRun = () => {
-  app.listen(dotEnv.portNumber, () => {
-    console.log(`The server is running on the port ${dotEnv.portNumber}`);
-  });
-};
