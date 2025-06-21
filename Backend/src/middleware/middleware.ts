fd61803107ea5f8080fb51d0e00bd27bdc9d0ec3
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-06-21 17:50:15 +0545

    added middle ware

---
+++ Backend/src/middleware/middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+class Middleware {
+  static isLoggedIn(req: Request, res: Response) {
+    // steps
+    // check if tooken
+    // verify the tooken
+  }
+}
+
+export default Middleware;
