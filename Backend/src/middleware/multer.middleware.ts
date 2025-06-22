62a28bb2aff8bdd2adb943df7e86bf9681d20fbd
Nirmal Chhetri <chhetrinirmal765@gmail.com>
2025-06-22 17:19:49 +0545

    added multter for file handling

---
+++ Backend/src/middleware/multer.middleware.ts
@@ -0,0 +1,23 @@
+import { Request } from "express";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  // this function dictates where to store the uploaded file
+
+  // cb is the callback function
+
+  destination: (req: Request, file: Express.Multer.File, cb: any) => {
+    cb(null, "./src/storage");
+
+    // takes two arg if error comes what to do
+
+    // next if files comes then what to do
+  },
+
+  // this function is concerned with how to name the uploaded file
+  filename: (req: Request, file: Express.Multer.File, cb: any) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+export { multer, storage };
